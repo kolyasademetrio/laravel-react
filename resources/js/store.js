b832d12eddb5f3de77cb9e6d311a1cfc364eaaf9
kolyasademetrio <kolyasademetrio@gmail.com>
2019-02-22 08:40:51 +0200

    redux bug fixed

--- resources/js/store.js
+++ resources/js/store.js
@@ -1,8 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 
-const store = createStore( rootReducer, applyMiddleware( logger ) );
+
+const store = createStore( rootReducer, composeWithDevTools(applyMiddleware(thunk, logger)) );
 
 export default store;

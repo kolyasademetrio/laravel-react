c393e6d6f6ac4772e1c9d20a802508e69a2e7580
kolyasademetrio <kolyasademetrio@gmail.com>
2019-05-22 00:18:52 +0300

    from home last

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,6 @@ Route::middleware(['guest'])->group(function(){
     Route::post('/login', 'Auth\LoginController@login');
 });
 
-<<<<<<< HEAD
-Route::middleware(['auth'])->group(function(){
-    //dd('is logged in');
-    /*Route::get('/account', 'AccountController@index')->name('account');
-    Route::get('/register', 'AccountController@index')->name('account');
-    Route::get('/login', 'AccountController@index')->name('login');*/
-=======
 Route::group(['middleware' => 'auth'], function(){
     Route::get('/logout', function(){
         \Auth::logout();
@@ -42,7 +35,6 @@ Route::group(['middleware' => 'auth'], function(){
         Route::get('/products/categories/edit/{id}', 'Admin\ProductsCategoriesController@editCategory')->where('id', '\d+')->name('admin.products.categories.edit');
         Route::delete('/products/categories/delete', 'Admin\ProductsCategoriesController@deleteCategory')->name('admin.products.categories.delete');
     });
->>>>>>> f5b7815720ee7437841faf669054d34ae7d8553f
 });
 
 Route::get('/{path?}', function () {
